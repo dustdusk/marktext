349ebe972b460152a09c88499734249704b0849c
陳傑弘 <dustdusk@digiwin.com>
2024-01-26 19:20:41 +0800

    Fixed the issue where including Chinese characters in the image path on the Windows would cause image reading failure.

--- src/renderer/util/clipboard.js
+++ src/renderer/util/clipboard.js
@@ -17,7 +17,7 @@ export const guessClipboardFilePath = () => {
     const result = getClipboardFiles()
     return Array.isArray(result) && result.length ? result[0] : ''
   } else if (isWindows) {
-    const rawFilePath = remoteClipboard.read('FileNameW')
+    const rawFilePath = remoteClipboard.readBuffer('FileNameW').toString('utf16le')
     const filePath = rawFilePath.replace(new RegExp(String.fromCharCode(0), 'g'), '')
     return filePath && typeof filePath === 'string' ? filePath : ''
   } else {
